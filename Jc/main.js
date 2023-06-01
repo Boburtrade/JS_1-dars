b33fd70376befcea67b9746a940cafb97403fa45
Maxmudjonov Boburjon <boburmahmudjonov484@gmail.com>
2023-06-01 21:50:49 +0500

    Bu JS 1-dars kodlari
    Yuklandi

---
+++ Jc/main.js
@@ -0,0 +1,61 @@
+/*console.log("4+((4*4)/)-4");*/
+/*alert("Saytimizga hush kelibsiz!");*/
+/*document.write("Men bodyda korinaman!");*/
+
+/*let x=2;
+let y=5;
+x=10;*/
+
+/*let  x=2,y=5,z=10;*/
+/*let color = "#f1f1f1"*/
+
+/*let salom = "Assalom Alekum"*/
+/*document.write(salom);*/
+
+/* data-types: number,string */
+
+/*let raqam=5;*/
+/*let soz = "JavaScript";*/
+
+/*document.write(typeof soz);*/
+
+/* JS arifmetik hisblash */
+
+/*let x=5;*/
+/*x+=5;*/  /* x=5+5 */
+/*x-=2;*/ /* x=5-2 */
+/*x*=5;*/ /* x=5*5 */
+/*x/=2;*/ /* x=5/2 */
+/*let qoldiq = x%4;*/ /* qoldiq chiqarish */
+/*let daraja = x**5;*/ /* daraja chiqarish */
+/* ++x; */
+/*--x;*/
+/*x++;*/
+/*x--;*/
+/*document.write(x+"5");*/
+
+/* if => agar */
+/* else = u holda */
+
+/*
+if ("Salom" == "Salom"){
+    console.log("Ular teng!");
+}
+else{
+    console.log("Ular teng emas!");
+}
+*/
+
+/* if => agar */
+/* else if => yoki bolmasa */
+/* else => u holda */
+
+if (45 == 46){
+    console.log("Bu son 45ga teng!");
+}
+else if (45 == 45){
+    console.log("Endi bu son 45ga teng!");
+}
+else{
+    console.log("Ikkala son ham 45ga teng emas!");
+}
